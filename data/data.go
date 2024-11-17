b2f4ab428541675acf260ef24553f1b2e94b0d6d
jmuchiri <mmjames451@gmail.com>
2024-11-17 23:04:53 +0300

    update: add logic to return combinations of unique paths

--- data/data.go
+++ data/data.go
@@ -20,7 +20,6 @@ func NewGraph() *Graph {
 }
 
 
-
 func (g *Graph) BFS(start, end string) [][]string {
 	allPaths := [][]string{}
 	qeue := [][]string{{start}}
@@ -30,7 +29,7 @@ func (g *Graph) BFS(start, end string) [][]string {
 		qeue = qeue[1:]
 		node := path[len(path)-1]
 	
-		if node == end && utils.IsUnique(allPaths, path[1:len(path)-1]){
+		if node == end {
 			allPaths = append(allPaths, path)
 		} else {
 			for _, neighbour := range g.Node[node] {
@@ -44,6 +43,51 @@ func (g *Graph) BFS(start, end string) [][]string {
 
 	}
 
-	return allPaths
+	return findMaxCompatiblePaths(allPaths)
+}
 
+// Simple helper to check if a path is compatible with a set of paths
+func isCompatibleWithAll(path []string, pathSet [][]string) bool {
+    // For each path in our current set
+    for _, existingPath := range pathSet {
+        // Check middle nodes (exclude start/end)
+        for i := 1; i < len(path)-1; i++ {
+            for j := 1; j < len(existingPath)-1; j++ {
+                if path[i] == existingPath[j] {
+                    return false
+                }
+            }
+        }
+    }
+    return true
+}
+
+// Simplified version to find maximum compatible paths
+func findMaxCompatiblePaths(paths [][]string) [][]string {
+    maxSet := [][]string{}
+    
+    // Try each path as a starting point
+    for i := 0; i < len(paths); i++ {
+        currentSet := [][]string{paths[i]}
+        
+        // Try to add each remaining path
+        for j := 0; j < len(paths); j++ {
+            if i == j {
+                continue
+            }
+            
+            // If this path is compatible with all current paths, add it
+            if isCompatibleWithAll(paths[j], currentSet) {
+                currentSet = append(currentSet, paths[j])
+            }
+        }
+        
+        // Update maxSet if we found a larger set
+        if len(currentSet) > len(maxSet) {
+            maxSet = make([][]string, len(currentSet))
+            copy(maxSet, currentSet)
+        }
+    }
+    
+    return maxSet
 }
