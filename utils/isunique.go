e039ee27dc1d9eb7c35f1df3d8d313a61c54ac9b
jmuchiri <mmjames451@gmail.com>
2024-11-18 10:58:37 +0300

    refactor: change function to work with the findmaxcompatiblepaths function

--- utils/isunique.go
+++ utils/isunique.go
@@ -1,14 +1,16 @@
 package utils
 
-func IsUnique(allArr [][]string, test []string) bool {
-	for _, arr := range allArr {
-		for _, val := range arr {
-			for _, n := range test {
-				if val == n {
-					return false
-				}
-			}
-		}
-	}
-	return true
+func IsUnique(test []string, allArr [][]string) bool {
+    for _, existingPath := range allArr {
+        // Check middle nodes (exclude start/end)
+        for i := 1; i < len(test)-1; i++ {
+            for j := 1; j < len(existingPath)-1; j++ {
+                if test[i] == existingPath[j] {
+                    return false
+                }
+            }
+        }
+    }
+    return true
+
 }
