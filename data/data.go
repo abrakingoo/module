6ad34a5932e582e9a61afc4593a529aa99c4ed78
wnjuguna <wm453856@gmail.com>
2024-11-18 18:18:19 +0300

    refactor: Make changes to cater for good practices

--- data/data.go
+++ data/data.go
@@ -1,28 +1,27 @@
 package data
 
 type AntFarm struct {
-    Ants int //number of ants in the file 
-    Start int //the start room
-    End int // the end room
-    Turns int // number of turns 
-    Filedata []string //the whole filedata
-    Rooms []string 
-    Tunnels []string
-    Paths []string
+	Ants     int      //number of ants in the file
+	Start    int      //the start room
+	End      int      // the end room
+	Turns    int      // number of turns
+	Filedata []string //the whole filedata
+	Rooms    []string
+	Tunnels  []string
+	Paths    []string
 }
 
 func NewFarm() *AntFarm {
-    return &AntFarm{}
+	return &AntFarm{}
 }
 
 type Ant struct {
-	Index int
-	Target int //target path
+	Index       int
+	Target      int //target path
 	TargetIndex int // the room of the target path
-	Iteration int // the specific turn
+	Iteration   int // the specific turn
 }
 
 func NewAnt() *Ant {
 	return &Ant{}
 }
-
