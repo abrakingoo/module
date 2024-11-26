a99eec84857b792cc845cdcf428aff58893469af
jmuchiri <mmjames451@gmail.com>
2024-11-26 22:19:08 +0300

    refactor: change file reading function to read multiple file format

--- data/filedata.go
+++ data/filedata.go
@@ -9,55 +9,44 @@ import (
 )
 
 func GetFileData(file []byte, farm *AntFarm) {
-	var (
-		start   string
-		end     string
-		rooms   []string
-		tunnels []string
-	)
-	
 	fileData := strings.Split(strings.TrimSpace(string(file)), "\n")
+	farm.Filedata = fileData
+
 	ants, err := strconv.Atoi(fileData[0])
 	utils.CheckError(err)
 	// if number of ants is 0
 	if ants <= 0 {
 		log.Fatal("ERROR: invalid number of ants")
 	}
+	farm.Ants = ants
 	// exclude number of ants
+	
 	if !utils.CheckStartAndEnd(fileData[1:]) {
-		log.Fatal("ERROR: invalid file format, missing ##start and ##end in file")
+		log.Fatal("ERROR: invalid file format, missing or repeated ##start and ##end values in the present file")
 	}
 
 	for i := 1; i < len(fileData); i++ {
 		line := fileData[i]
-		if line == "" || strings.Contains(line , "#") {
+		if line == ""  {
 			continue
 		}
-		if line == "##start" {
-			start = "" // index of the start room
-		}
-
-		if line[0] == '#' && line != "##end" {
-			continue
+		if strings.Contains(line, "#") {
+			if line == "##start" {
+				farm.Start = strings.Split(fileData[i+1], " ")[0]
+				continue
+			} else if line == "##end" {
+				farm.End = strings.Split(fileData[i+1], " ")[0]
+				continue
+			} else {
+				continue
+			}
 		}
 
 		if strings.Contains(line, "-") {
-			rooms = append(rooms, line)
+			farm.Tunnels = append(farm.Tunnels, line)
+			continue
 		}
-
-		if line == "##end" {
-			end = ""
-			 // index of the end room
-		}
-		rooms = append(rooms, line) // Add the room name
+		farm.Rooms = append(farm.Rooms, line) // Add the room name
 	}
 
-	
-
-	farm.Ants = ants
-	farm.Filedata = fileData
-	farm.Start = start
-	farm.End = end
-	farm.Rooms = rooms
-	farm.Tunnels = tunnels
 }
